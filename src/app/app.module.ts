7fb12927b9f9f19a34bdd7c98dcad56a7aac0749
Radoslav Genchev <r.genchew@gmail.com>
2017-08-15 10:04:19 +0200

    Set up the login page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { NewsPage } from '../pages/news/news';
 import { VideoPage } from '../pages/video/video';
 import { TabsPage } from '../pages/tabs/tabs';
 import { IntroPage } from '../pages/intro/intro';
+import { LoginPage } from '../pages/login/login';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
@@ -19,7 +20,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     NewsPage,
     VideoPage,
     TabsPage,
-    IntroPage
+    IntroPage,
+    LoginPage
   ],
   imports: [
     BrowserModule,
@@ -32,7 +34,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     NewsPage,
     VideoPage,
     TabsPage,
-    IntroPage
+    IntroPage,
+    LoginPage
   ],
   providers: [
     StatusBar,
